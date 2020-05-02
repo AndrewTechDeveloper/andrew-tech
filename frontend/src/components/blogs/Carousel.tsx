4d53eac5092b272683cdacd6c6038fa9a147c7ea
Andrew <andrew.charcopin@gmail.com>
2020-05-02 10:38:52 +0900

    fix carousel

--- frontend/src/components/blogs/Carousel.tsx
+++ frontend/src/components/blogs/Carousel.tsx
@@ -17,22 +17,20 @@ const style = {
   }
 }
 export const CarouselTitle: React.FC<CarouselProps> = ({ data, history }) => (
-  <MDBContainer className="p-0 m-0 mw-100">
-    <MDBCarousel activeItem={1} length={data && data.length} showControls={true} showIndicators={true} className="z-depth-1">
-      <MDBCarouselInner>
-        {data && data.map(blog => (
-          <MDBCarouselItem type="button" itemId="1" key={blog.id} onClick={() => history.push(`blogs/${blog.id}`)}>
-            <MDBView>
-              <img alt='carousel' className="d-block m-auto" src={blog.og_image} style={style.carousel.img} />
-              <MDBMask overlay="black-light" />
-            </MDBView>
-            <MDBCarouselCaption>
-              <h3 className="h3-responsive">{blog.title}</h3>
-              <p>{blog.description}</p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-        ))}
-      </MDBCarouselInner>
-    </MDBCarousel>
-  </MDBContainer>
+  <MDBCarousel activeItem={1} length={data && data.length} showControls={true} showIndicators={true} className="z-depth-1">
+    <MDBCarouselInner>
+      {data && data.map(blog => (
+        <MDBCarouselItem type="button" itemId="1" key={blog.id} onClick={() => history.push(`blogs/${blog.id}`)}>
+          <MDBView>
+            <img alt='carousel' className="d-block m-auto" src={blog.og_image} style={style.carousel.img} />
+            <MDBMask overlay="black-light" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h3 className="h3-responsive">{blog.title}</h3>
+            <p>{blog.description}</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+      ))}
+    </MDBCarouselInner>
+  </MDBCarousel>
 )
