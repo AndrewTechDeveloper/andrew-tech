2db6f7f9429f54038ff5f7ef3ab347fb2206db81
Andrew <andrew.charcopin@gmail.com>
2020-05-11 21:54:24 +0900

    remove files

--- frontend/src/frontend/src/components/layouts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <footer className="page-footer font-small black">
-      <div className="footer-copyright text-center py-3">© 2020 Copyright: Andrew-tech</div>
-    </footer>
-  )
-}
--- frontend/src/frontend/src/react-app-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface Window {
-  INITIAL_REDUX_STATE: any
-  __PRELOADED_STATE__: any
-  snapSaveState: any
-}
-declare module 'react-twitter-embed';
-declare module 'react-snapshot';
